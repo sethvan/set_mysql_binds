2336a31b9f75be5f26abfb0c39a4cf4f363cc89a
Sethvan <sethvan@yahoo.com>
2023-02-04 22:44:41 -0700

    set_msql_binds.h

---
+++ include/set_mysql_binds.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDED_SET_MYSQL_BINDS_H
+#define INCLUDED_SET_MYSQL_BINDS_H
+
+#include "binds.hpp"
+#include "createDBTableBinds.h"
+#include "getDBTables.h"
+#include "sqlTypes.hpp"
+#include "typeCreationStrings.h"
+#include "utilities.h"
+
+#endif  // INCLUDED_SET_MYSQL_BINDS_H
