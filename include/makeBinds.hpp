080863f250157ad0cc036e5fe8f5b81abef1c790
Sethvan <sethvan@yahoo.com>
2023-02-10 17:34:33 -0700

    Refactored makeBinds.hpp.

--- include/makeBinds.hpp
+++ include/makeBinds.hpp
@@ -7,61 +7,43 @@
 
 namespace set_mysql_binds {
 
-    struct ElementBase {
-        std::string_view name;
-        unsigned long buffer_size;
-        ElementBase( std::string_view _name, unsigned long _buffer_size = 0 )
-            : name( _name ), buffer_size( _buffer_size ) {
-        }
-
-        virtual ~ElementBase() = default;
-
-        virtual std::unique_ptr<InputCType> makeInput() = 0;
-
-        virtual std::unique_ptr<OutputCType> makeOutput() = 0;
-    };
-
     template <MysqlInputType T>
-    struct Element : public ElementBase {
+    struct Element {
         using inType = BindType<T>::inType;
         using outType = BindType<T>::outType;
 
-        Element( std::string_view _name, unsigned long _buffer_size = 0 ) : ElementBase( _name, _buffer_size ) {
+        std::string_view name;
+        unsigned long buffer_size;
+
+        Element( std::string_view _name, unsigned long _buffer_size = 0 )
+            : name( _name ), buffer_size( +_buffer_size ) {
         }
 
-        std::unique_ptr<InputCType> makeInput() override {
+        std::unique_ptr<InputCType> makeInput() {
             return std::make_unique<inType>( name, buffer_size );
         }
 
-        std::unique_ptr<OutputCType> makeOutput() override {
+        std::unique_ptr<OutputCType> makeOutput() {
             return std::make_unique<outType>( name, buffer_size );
         }
     };
 
     template <MysqlInputType T>
-    std::unique_ptr<ElementBase> Bind( std::string_view _name, unsigned long _buffer_size = 0 ) {
+    std::unique_ptr<Element<T>> Bind( std::string_view _name, unsigned long _buffer_size = 0 ) {
         return std::make_unique<Element<T>>( _name, _buffer_size );
     }
 
     template <typename... Ptrs>
     Binds<InputCType> makeInputBinds( Ptrs&&... ptrs ) {
-        std::vector<std::unique_ptr<ElementBase>> baseVec;
-        ( baseVec.emplace_back( std::forward<Ptrs>( ptrs ) ), ... );
-
         std::vector<std::unique_ptr<InputCType>> vec;
-        std::for_each( baseVec.begin(), baseVec.end(),
-                       [&]( auto& element ) { vec.emplace_back( element->makeInput() ); } );
+        ( vec.emplace_back( std::forward<std::unique_ptr<InputCType>>( ptrs->makeInput() ) ), ... );
         return Binds<InputCType>( std::move( vec ) );
     }
 
     template <typename... Ptrs>
     Binds<OutputCType> makeOutputBinds( Ptrs&&... ptrs ) {
-        std::vector<std::unique_ptr<ElementBase>> baseVec;
-        ( baseVec.emplace_back( std::forward<Ptrs>( ptrs ) ), ... );
-
         std::vector<std::unique_ptr<OutputCType>> vec;
-        std::for_each( baseVec.begin(), baseVec.end(),
-                       [&]( auto& element ) { vec.emplace_back( element->makeOutput() ); } );
+        ( vec.emplace_back( std::forward<std::unique_ptr<OutputCType>>( ptrs->makeOutput() ) ), ... );
         return Binds<OutputCType>( std::move( vec ) );
     }
 
